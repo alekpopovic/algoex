25c344d48fe80bc102902ab219ef00fddbfbbc5c
Aleksandar Popovic <aleksandar.popovic@spothopperapp.com>
2025-01-22 20:30:32 +0100

    init

---
+++ lib/algoex.ex
@@ -0,0 +1,18 @@
+defmodule Algoex do
+  @moduledoc """
+  Documentation for `Algoex`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Algoex.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
